a6eb43430c2a8e1ed57957376fb767dd56b8b051
Sam Spilsbury <smspillaz@gmail.com>
2021-05-10 16:34:45 +0300

    components: Remove querying of location for root pages
    Instead have components that are root pages pass this to Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -58,51 +58,36 @@ Nav.propTypes = {
   ),
 }
 
-const toplevelPages = [
-  `${__PATH_PREFIX__}/`,
-  `${__PATH_PREFIX__}/posts`,
-  `${__PATH_PREFIX__}/projects`,
-  `${__PATH_PREFIX__}/research`,
-  `${__PATH_PREFIX__}/hire`,
-]
-
 const components = {
   bio: PageBio,
   link: Link,
 }
 
-const Layout = ({ location, title, children, menuLinks }) => {
-  const isRootPath = !!toplevelPages.filter(p => p === location.pathname).length
-  let header
-
-  if (isRootPath) {
-    header = (
-      <div>
-        <h1 className="main-heading" style={{ textAlign: "center" }}>
-          <Link to="/">{title}</Link>
-        </h1>
-        <BackgroundAnimation
-          style={{
-            margin: "-80px 0 -80px 0",
-            zIndex: 0,
-            width: "100%",
-            height: "300px",
-          }}
-        ></BackgroundAnimation>
-        <Nav menuLinks={menuLinks} />
-      </div>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+const Layout = ({ title, children, menuLinks, root = false }) => {
+  const header = root ? (
+    <div>
+      <h1 className="main-heading" style={{ textAlign: "center" }}>
+        <Link to="/">{title}</Link>
+      </h1>
+      <BackgroundAnimation
+        style={{
+          margin: "-80px 0 -80px 0",
+          zIndex: 0,
+          width: "100%",
+          height: "300px",
+        }}
+      ></BackgroundAnimation>
+      <Nav menuLinks={menuLinks} />
+    </div>
+  ) : (
+    <Link className="header-link-home" to="/">
+      {title}
+    </Link>
+  )
 
   return (
     <MDXProvider components={components}>
-      <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <div className="global-wrapper" data-is-root-path={root}>
         <header className="global-header">{header}</header>
         <main>{children}</main>
         <footer>
