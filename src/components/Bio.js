181c3fcec155add4729706637f3c1109fe05a7c5
Sam Spilsbury <smspillaz@gmail.com>
2021-05-10 10:03:18 +0300

    components: Rewrite Bio to avoid use of useStaticQuery

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,47 +6,48 @@
  */
 
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 const Bio = () => {
-  const data = useStaticQuery(graphql`
-    query BioQuery {
-      site {
-        siteMetadata {
-          author {
-            name
-            summary
-          }
-          social {
-            twitter
+  return (
+    <StaticQuery
+      query={graphql`
+        query BioQuery {
+          site {
+            siteMetadata {
+              author {
+                name
+                summary
+              }
+            }
           }
         }
-      }
-    }
-  `)
-
-  // Set these values by editing "siteMetadata" in gatsby-config.js
-  const author = data.site.siteMetadata?.author
-
-  return (
-    <div className="bio">
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["AUTO", "WEBP", "AVIF"]}
-        src="../images/profile-pic.jpg"
-        width={50}
-        height={50}
-        quality={95}
-        alt="Profile picture"
-      />
-      {author?.name && (
-        <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
-        </p>
-      )}
-    </div>
+      `}
+      render={({ site }) => {
+        const author = site.siteMetadata?.author
+        return (
+          <div className="bio">
+            <StaticImage
+              className="bio-avatar"
+              layout="fixed"
+              formats={["AUTO", "WEBP", "AVIF"]}
+              src="../images/profile-pic.jpg"
+              width={50}
+              height={50}
+              quality={95}
+              alt="Profile picture"
+            />
+            {author?.name && (
+              <p>
+                Written by <strong>{author.name}</strong>{" "}
+                {author?.summary || null}
+              </p>
+            )}
+          </div>
+        )
+      }}
+    />
   )
 }
 
