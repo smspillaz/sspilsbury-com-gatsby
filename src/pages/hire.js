8e06f5796ca1a5e8a95e13aaf4d1bf05eadd17c1
Sam Spilsbury <smspillaz@gmail.com>
2021-05-10 13:48:45 +0300

    pages: Add hire and research pages

---
+++ src/pages/hire.js
@@ -0,0 +1,52 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+
+import Layout from "../components/Layout"
+import Seo from "../components/SEO"
+
+const HirePageIndex = ({ data, location }) => {
+  const post = data.mdx
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const menuLinks = data.site.siteMetadata.menuLinks
+
+  return (
+    <Layout location={location} title={siteTitle} menuLinks={menuLinks}>
+      <Seo
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <article
+        className="blog-post"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <MDXRenderer>{post.body}</MDXRenderer>
+      </article>
+    </Layout>
+  )
+}
+
+export default HirePageIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        menuLinks {
+          name
+          link
+          nav
+        }
+      }
+    }
+    mdx(frontmatter: { name: { eq: "hire" } }) {
+      id
+      body
+      frontmatter {
+        title
+      }
+    }
+  }
+`
